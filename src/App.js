563e50855e83a9cb43ef5e146da52f81e9496991
Travis <tjlemperle@gmail.com>
2017-09-23 22:01:51 -0600

    added sidebar buttons

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
 import './styles/App.css';
 import UserBar from './components/UserBar';
+import maleImage from './assets/male-face.jpeg'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
           <nav className="header-bar"></nav>
-          <div className="sidebar-nav"></div>
+          <div className="sidebar-nav">
+            <div className="sidebar-profile">
+              <div className="sidebar-profileContainer">
+                <img id="sidebar-profilePic" src={maleImage} alt=""/>
+              </div>
+              <div>
+                <p id="sidebar-profileName">Travis L</p>
+                <p id="sidebar-profileTitle">Team Peri</p>
+              </div>
+            </div>
+            <div className="sidebar-btns">
+              <button type="submit" className="buttonStyle">Outbound Call</button>
+              <button type="submit" className="buttonStyle">Question</button>
+              <button type="submit" className="buttonStyle">Supervisor Call</button>
+              <button type="submit" className="buttonStyle">Technical Difficulty</button>
+            </div>
+          </div>
           <div className="main-content">
               <div className="supervisor-section">
                   <UserBar />
